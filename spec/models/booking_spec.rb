dec0daebf7a1454fd7ef210ac5d09233b8142fb5
dipto0321 <diptokmk47@gmail.com>
2018-12-21 10:19:32 +0600

    Add spec for
        * Booking and Passenger model
        * in spec, check the relations between other models (associations)
        * Also check valid data

--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Booking, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Associations' do
+    it { should belong_to(:flight) }
+    it { should have_many(:passengers) }
+    it { should accept_nested_attributes_for(:passengers) }
+  end
+
+  describe 'Validations' do
+    it { should validate_presence_of(:flight_id) }
+  end
 end
--- spec/models/passenger_spec.rb
+++ spec/models/passenger_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Passenger, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:booking) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:email) }
+  end
 end
