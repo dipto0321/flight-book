8eef9185ee52d4ba63beaa3e7dde69fba07a33f2
dipto0321 <diptokmk47@gmail.com>
2018-12-21 10:05:12 +0600

    Add spec for flight model

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,6 +3,8 @@ class Flight < ApplicationRecord
   belongs_to :arival_airport, class_name: 'Airport'
   has_many :bookings
 
+  validates :departure_airport_id, :arival_airport_id, :start, :duration, presence: true
+
 
   def self.search_flight(departure_airport, arival_airport)
     Flight.where("departure_airport_id=? AND arival_airport_id= ?", departure_airport, arival_airport)
--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Flight, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Associations' do
+    it { should belong_to(:departure_airport).class_name('Airport') }
+
+    it { should belong_to(:arival_airport).class_name('Airport') }
+
+    it { should have_many(:bookings) }
+  end
+
+  describe 'Validations' do
+    it { should validate_presence_of(:departure_airport_id) }
+    it { should validate_presence_of(:arival_airport_id) }
+    it { should validate_presence_of(:start) }
+    it { should validate_presence_of(:duration) }
+  end
 end
